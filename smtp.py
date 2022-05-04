52e56bc547616a61719645726cbe41c7a515707a
Kelvin <80852728+kelvinnn25@users.noreply.github.com>
2022-05-03 21:42:03 -0400

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -1,5 +1,6 @@
 import smtplib, ssl
 import pandas as pd
+import numpy as np
 
 #assumes it is located in first sheet of your worbook indexed 
 df = pd.read_excel('emails.xlsx', sheetname=0) 
@@ -16,8 +17,14 @@ server.login('email', 'password')
 
 #from = your email address, 
 for reciever in emailList:
-    for msg in messageList: 
+    for msg in messageList:
+        msgPointer = 0 
         server.sendmail("from", reciever, msg)
+        msgPointer +=1
+
     server.quit()
+df = pd.read_excel('messages.xlsx') #Here we read the excel
+df.iloc[11:1001,:1] = np.nan #Here we are selecting from the first column upto the first row only and from the 11th row up to the 1000 and making the values null.
+df.to_excel('location_of_new_file.xlsx') #Here we are saving the excel with the modifications we made.
 
 
